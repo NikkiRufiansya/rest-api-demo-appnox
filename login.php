c9537a64a6472f441aa3b1b13e45c91b6e7c32fc
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2023-10-24 08:32:01 +0700

    update users

--- login.php
+++ login.php
@@ -19,10 +19,26 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit();
 }
 
+// Data pengguna dalam bentuk array
+$users = [
+    [
+        'email' => 'niki@rml.co.id',
+        'password' => '123456',
+    ],
+    [
+        'email' => 'putra@rml.co.id',
+        'password' => '123456',
+    ],
+    [
+        'email' => 'rizky@rml.co.id',
+        'password' => '123456'
+    ]
+    // Tambahkan pengguna lain di sini
+];
+
 // Mendapatkan data dari input HTTP POST
 $json = file_get_contents('php://input');
 $input = json_decode($json);
-
 
 // Pastikan data yang diperlukan ada dalam input
 if (!isset($input->email) || !isset($input->password)) {
@@ -33,14 +49,19 @@ if (!isset($input->email) || !isset($input->password)) {
     exit();
 }
 
-// Cuma data mock/dummy, bisa diganti dengan data dari database
-$user = [
-    'email' => 'niki@rml.co.id',
-    'password' => '123456'
-];
+$authenticated = false;
+$selectedUser = null;
 
-// Verifikasi email dan password
-if ($input->email !== $user['email'] || $input->password !== $user['password']) {
+// Loop melalui data pengguna dan verifikasi email dan password
+foreach ($users as $user) {
+    if ($input->email === $user['email'] && $input->password === $user['password']) {
+        $authenticated = true;
+        $selectedUser = $user;
+        break;
+    }
+}
+
+if (!$authenticated) {
     http_response_code(401);
     echo json_encode([
         'message' => 'Email atau password tidak sesuai'
@@ -51,20 +72,18 @@ if ($input->email !== $user['email'] || $input->password !== $user['password']) 
 // Inisialisasi payload
 $expired_time = time() + (15 * 600000000);
 $payload = [
-    'email' => $input->email,
+    'email' => $selectedUser['email'],
     'exp' => $expired_time
 ];
-
 
 $access_token = JWT::encode($payload, $_ENV['ACCESS_TOKEN_SECRET'], 'HS256');
 $refresh_token = JWT::encode($payload, $_ENV['REFRESH_TOKEN_SECRET'], 'HS256');
 
-
-
 echo json_encode([
     'status' => 'success',
-    'access_token' => $access_token, 
+    'access_token' => $access_token,
+    'data' => [
+        "email" => $input->email
+    ]
 ]);
-
-
 ?>
